5da68d8cc9868d59607881e1100d878470a90746
Chris Cosentino <chriscosentino@hotmail.com>
2020-06-14 20:32:36 -0400

    Updates

---
+++ precache-manifest.4d23692ffb20fdc9945f1f1b12ba638a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476b17e142bb0d7d7ac6d533282ee29b",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "2b17435c666eff5d5427",
+    "url": "/corona-tracker/static/css/main.946826ac.chunk.css"
+  },
+  {
+    "revision": "db92b5393faff0f9ea49",
+    "url": "/corona-tracker/static/js/2.d48d7d17.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/corona-tracker/static/js/2.d48d7d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b17435c666eff5d5427",
+    "url": "/corona-tracker/static/js/main.92593b7a.chunk.js"
+  },
+  {
+    "revision": "7e7c08e4a6218267afe9",
+    "url": "/corona-tracker/static/js/runtime-main.1b83197d.js"
+  }
+]);
